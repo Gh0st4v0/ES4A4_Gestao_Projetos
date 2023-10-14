d0ee58cec00070eb2b3d30ebcb0405a5d9f2ce0b
Gabriel Cesar <bielczr@outlook.pt>
2023-10-14 03:53:11 -0300

    CMYT - 0.0.2
    Modificação geral no layout da pagina, adições de novas funções e widgets.

--- CMYT/js/app.js
+++ CMYT/js/app.js
@@ -1,4 +1,3 @@
-// Dados simulados de projetos do usuário
 const userProjects = [
     { id: 1, name: 'Projeto A', description: 'Descrição do Projeto A' },
     { id: 2, name: 'Projeto B', description: 'Descrição do Projeto B' }
@@ -32,57 +31,13 @@ const userProjects = [
     `;
   }
   
-  function showTasks() {
-    const content = document.getElementById('content');
-    content.innerHTML = `
-      <div class="activities-table">
-        <h2>Atividades Atribuídas</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Atividade</th>
-              <th>Prazo</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Atividade 1</td>
-              <td>15/10/2023</td>
-            </tr>
-            <tr>
-              <td>Atividade 2</td>
-              <td>18/10/2023</td>
-            </tr>
-            <!-- Adicionar mais linhas conforme necessário -->
-          </tbody>
-        </table>
-      </div>
-      <div class="activities-table">
-        <h2>Atividades Não Concluídas</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Atividade</th>
-              <th>Prazo</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Atividade 3</td>
-              <td>20/10/2023</td>
-            </tr>
-            <tr>
-              <td>Atividade 4</td>
-              <td>22/10/2023</td>
-            </tr>
-            <!-- Adicionar mais linhas conforme necessário -->
-          </tbody>
-        </table>
-      </div>
-    `;
-  }
-
-
+  const activityData = [
+    { title: 'Atividade 1', start: '2023-10-15', end: '2023-10-16', description: 'Descrição da atividade 1' },
+    { title: 'Atividade 2', start: '2023-10-18', end: '2023-10-20', description: 'Descrição da atividade 2' },
+    { title: 'Atividade 3', start: '2023-11-10', end: '2023-11-12', description: 'Descrição da atividade 3' }
+    // Adicione mais atividades conforme necessário
+  ];
+  
   function initializeCalendar() {
     $('#calendar-container').fullCalendar({
       header: {
@@ -98,18 +53,6 @@ const userProjects = [
     });
   }
   
-  function showProjects() {
-    // ... (códigos anteriores) ...
-  }
-  
-  function showProjectDetails(projectId) {
-    // ... (códigos anteriores) ...
-  }
-  
-  function showTasks() {
-    // ... (códigos anteriores) ...
-  }
-  
   function showMembers() {
     // Lógica para mostrar a lista de membros na seção de conteúdo
   }
@@ -117,7 +60,7 @@ const userProjects = [
   function showCommunication() {
     // Lógica para mostrar a comunicação na seção de conteúdo
   }
-
+  
   function showUserTasks() {
     // Lógica para mostrar as atividades atribuídas ao usuário
   }
